9bac92dca10832626e6db1798d74b9888173f7db
gryunlim <gryunlim@naver.com>
2025-09-18 11:23:55 +0900

    chor: 초기 설정(폴더 기본 파비콘 포함)

---
+++ src/main/java/com/devdotdone/ddd/controller/DBController.java
@@ -0,0 +1,11 @@
+package com.devdotdone.ddd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DBController {
+
+	public String home() {
+		return "home";
+	}
+}
