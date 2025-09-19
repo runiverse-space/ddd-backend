2bdf0dff6f172cad11bfaaef8a89ab606fe57d00
gryunlim <gryunlim@naver.com>
2025-09-19 17:03:09 +0900

    feat: project 컨트롤러 작성

---
+++ src/main/java/com/devdotdone/ddd/controller/ProjectController.java
@@ -0,0 +1,34 @@
+package com.devdotdone.ddd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devdotdone.ddd.dto.project.Project;
+import com.devdotdone.ddd.service.ProjectService;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Slf4j
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+
+  @Autowired
+  private ProjectService projectService;
+
+  @PostMapping("/project-create")
+  public Project projectCreate(@RequestBody Project project) {
+      log.info(project.toString());
+      
+      projectService.create(project);
+      Project dbProject= projectService.getProject(project.getProjectId());
+
+
+      return dbProject;
+  }
+  
+}
