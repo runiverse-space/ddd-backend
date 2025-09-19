8a871f525723b7c6deb4786b2338b8b41631a08a
dlgudwn55 <dkrlekeheoek@naver.com>
2025-09-19 17:42:06 +0900

    feat: ProjectActivityDao 작성

--- src/main/java/com/devdotdone/ddd/dao/ProjectActivityDao.java
+++ src/main/java/com/devdotdone/ddd/dao/ProjectActivityDao.java
@@ -1,8 +1,17 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.devdotdone.ddd.dto.project.ProjectActivity;
 
 @Mapper
 public interface ProjectActivityDao {
-
+  public int insertActivity(ProjectActivity projectActivity);
+  public ProjectActivity selectById(int activityId);
+  public List<ProjectActivity> selectByReceiver(int userId);
+  public int updateActivityStatus(String newStatus);
+  public int chageToRead();
+  public int delete(int activityId);
 }
