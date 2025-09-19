45f27facc1c24406db31b80c71c520094aa4b4c1
gryunlim <gryunlim@naver.com>
2025-09-19 17:04:11 +0900

    feat: projectservice 작성함

---
+++ src/main/java/com/devdotdone/ddd/service/ProjectService.java
@@ -0,0 +1,26 @@
+package com.devdotdone.ddd.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devdotdone.ddd.dao.ProjectDao;
+import com.devdotdone.ddd.dto.project.Project;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class ProjectService {
+  @Autowired
+  private ProjectDao projectDao;
+
+  public void create(Project project){
+    projectDao.insertProject(project);
+  }
+
+
+  public Project getProject(int projectId){
+    Project project = projectDao.selectProjectById(projectId);
+    return project;
+  }
+}
