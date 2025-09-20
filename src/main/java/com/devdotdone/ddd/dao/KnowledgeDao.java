334a60a5d2f5f144768088a09a81d95ee9f0a632
gryunlim <gryunlim@naver.com>
2025-09-21 00:36:17 +0900

    feat: knowledgeDao 추가

--- src/main/java/com/devdotdone/ddd/dao/KnowledgeDao.java
+++ src/main/java/com/devdotdone/ddd/dao/KnowledgeDao.java
@@ -1,8 +1,33 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.devdotdone.ddd.dto.knowledge.Knowledge;
 
 @Mapper
 public interface KnowledgeDao {
+  //생성
+  public int insertKnowledge(Knowledge knowledge);
+  //knowledgeId별 조회
+  public Knowledge selectKnowledgeByKnowledgeId(int knowledgeId);
+  //유저별 조회
+  public Knowledge selectKnowledgeByUserId(int userId);
+  //프로젝트별 조회
+  public Knowledge selectKnowledgeByProjectId(int projectId);
+
+  //수정
+  public int update(Knowledge knowledge);
+
+  //삭제
+  public int delete(int knowledgeId);
+
+  public int countAll();
+
+
+ // public List<Knowledge> selectByPage(Pager pager);
+
+
 
 }
