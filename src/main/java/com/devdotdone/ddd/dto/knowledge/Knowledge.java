e925bce3ff1fca219627cce84d6ec28693c20d06
gryunlim <gryunlim@naver.com>
2025-09-21 00:36:41 +0900

    feat: Knowledge dto 추가

--- src/main/java/com/devdotdone/ddd/dto/knowledge/Knowledge.java
+++ src/main/java/com/devdotdone/ddd/dto/knowledge/Knowledge.java
@@ -1,5 +1,21 @@
 package com.devdotdone.ddd.dto.knowledge;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
 public class Knowledge {
+  private int projectId;
+  private int userId;
+  private String knowledgeTitle;
+  private String knoledgeContent;
+  private String knowledgeUrl;
+
+  private MultipartFile battach;
+  private String kfattachoname;
+  private String kfattachsname;
+  private String kfattachtype;
+  private byte[] kfattachdata;
 
 }
