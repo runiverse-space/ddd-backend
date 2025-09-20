b4e65c94ffd61ab6411d83d0dfd315dcf9a53eca
gryunlim <gryunlim@naver.com>
2025-09-21 01:26:36 +0900

    feat: KnowledgeComment Dao, dto 추가함

--- src/main/java/com/devdotdone/ddd/dao/KnowledgeCommentDao.java
+++ src/main/java/com/devdotdone/ddd/dao/KnowledgeCommentDao.java
@@ -2,7 +2,19 @@ package com.devdotdone.ddd.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.devdotdone.ddd.dto.knowledge.KnowledgeComment;
+
 @Mapper
 public interface KnowledgeCommentDao {
+   public int insert(KnowledgeComment knowledgeComment);
 
+  public KnowledgeComment selectKnowledgeCommentByKnowledgeCommentId(int knowledgeCommentId);
+
+  //public List<knowledgeCommentId> selectByPage(Pager pager);
+
+  public int update(KnowledgeComment knowledgeComment);
+
+  public int delete(int knowledgeCommentId);
+
+  public int countAllKnowledgeComment();
 }
--- src/main/java/com/devdotdone/ddd/dto/knowledge/KnowledgeComment.java
+++ src/main/java/com/devdotdone/ddd/dto/knowledge/KnowledgeComment.java
@@ -1,5 +1,14 @@
 package com.devdotdone.ddd.dto.knowledge;
 
-public class KnowledgeComment {
+import java.sql.Date;
 
+import lombok.Data;
+
+@Data
+public class KnowledgeComment {
+    private int knowledgeCommentId;
+    private int knowledgeId;
+    private int userId;
+    private String knowledgeCommentContent;
+    private Date knowledgeCommentCreatedAt;
 }
