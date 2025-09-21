ff127ff1bac9631c973b3d30a24804b2587cdc7b
LeeHyeongju <a01028245126@gmail.com>
2025-09-21 22:32:00 +0900

    feat: ProjectTagDao 작성

--- src/main/java/com/devdotdone/ddd/dao/ProjectTagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/ProjectTagDao.java
@@ -1,8 +1,18 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.devdotdone.ddd.dto.tag.KnowledgeTag;
+import com.devdotdone.ddd.dto.tag.ProjectTag;
 
 @Mapper
 public interface ProjectTagDao {
-
+  // 프로젝트에 태그 추가
+  public int insertProjectTag(ProjectTag projectTag);
+  // 프로젝트에 포함된 태그 조회
+  public List<KnowledgeTag> findTags(int projectid);
+  // 프로젝트의 태그 제거
+  public int deleteTag(int projectid);
 }
