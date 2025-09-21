5eb519bd632d54c7db379c1bce8a9db3da5bb3a4
LeeHyeongju <a01028245126@gmail.com>
2025-09-21 22:34:40 +0900

    chore: UserTagDao 작성

--- src/main/java/com/devdotdone/ddd/dao/UserTagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/UserTagDao.java
@@ -1,8 +1,17 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.devdotdone.ddd.dto.tag.UserTag;
 
 @Mapper
 public interface UserTagDao {
-
+  // 프로젝트에 태그 추가
+  public int insertUserTag(UserTag projectTag);
+  // 프로젝트에 포함된 태그 조회
+  public List<UserTag> findTags(int userId);
+  // 프로젝트의 태그 제거
+  public int deleteTag(int userId);
 }
