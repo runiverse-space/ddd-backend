275e6f50f21536b3474d7b9bdc97da65ad7baad0
LeeHyeongju <a01028245126@gmail.com>
2025-09-21 22:30:03 +0900

    feat: KnowledgeTagDao 작성

--- src/main/java/com/devdotdone/ddd/dao/KnowledgeTagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/KnowledgeTagDao.java
@@ -1,8 +1,17 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.devdotdone.ddd.dto.tag.KnowledgeTag;
 
 @Mapper
 public interface KnowledgeTagDao {
-
+  // 지식에 태그 추가
+  public int insertKnowledgeTag(KnowledgeTag knowledgeTag);
+  // 지식에 포함된 태그 조회
+  public List<KnowledgeTag> findTags(int knowledgeId);
+  // 지식의 태그 제거
+  public int deleteTag(int knowledgeId);
 }
