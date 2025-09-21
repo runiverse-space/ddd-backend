a2cf8cadf204f6fc3c1980a5122cee82def04450
LeeHyeongju <a01028245126@gmail.com>
2025-09-21 22:35:01 +0900

    chore: 주석 정정

--- src/main/java/com/devdotdone/ddd/dao/UserTagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/UserTagDao.java
@@ -8,10 +8,10 @@ import com.devdotdone.ddd.dto.tag.UserTag;
 
 @Mapper
 public interface UserTagDao {
-  // 프로젝트에 태그 추가
+  // 사용자에 태그 추가
   public int insertUserTag(UserTag projectTag);
-  // 프로젝트에 포함된 태그 조회
+  // 사용자에 포함된 태그 조회
   public List<UserTag> findTags(int userId);
-  // 프로젝트의 태그 제거
+  // 사용자의 태그 제거
   public int deleteTag(int userId);
 }
