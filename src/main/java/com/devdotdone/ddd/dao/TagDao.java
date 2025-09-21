5fdee1ae134117bd380154cf6f1d34c823bb5bd1
LeeHyeongju <a01028245126@gmail.com>
2025-09-21 22:42:41 +0900

    feat: TagDao 작성

--- src/main/java/com/devdotdone/ddd/dao/TagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/TagDao.java
@@ -1,8 +1,24 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.devdotdone.ddd.dto.tag.Tag;
 
 @Mapper
 public interface TagDao {
-  
+  public int insertTag(Tag tag);
+
+  public Tag selectTagById(int tagId);
+
+  public Tag selectTagByTagName(String tagName);
+
+  public Tag selectTagByTagType(String tagType);
+
+  public List<Tag> selectAllTags();
+
+  public int updateTag(Tag tag);
+
+  public int deleteTag(String tagId);
 }
