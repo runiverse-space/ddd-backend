07e4668145014f34a8b457f24afb68a36bf7b89a
dlgudwn55 <dkrlekeheoek@naver.com>
2025-09-22 10:13:13 +0900

    feat: 회원가입 유효성 검사

--- src/main/java/com/devdotdone/ddd/controller/UsersController.java
+++ src/main/java/com/devdotdone/ddd/controller/UsersController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.devdotdone.ddd.dto.users.Users;
 import com.devdotdone.ddd.service.UsersService;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -25,7 +26,7 @@ public class UsersController {
   private UsersService usersService;
 
   @PostMapping("/create")
-  public Map<String, Object> create(Users users) throws IOException {
+  public Map<String, Object> create(@Valid Users users) throws IOException {
     log.info(users.toString());
 
     MultipartFile mf = users.getUfAttach();
