647b3129ee1a220a1d5899fff68693495a67bc7f
devbsu <devbsu@naver.com>
2025-09-22 19:23:51 +0900

    feat: 태그, 유저 태그 dao 추가

--- src/main/java/com/devdotdone/ddd/dao/TagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/TagDao.java
@@ -1,8 +1,33 @@
 package com.devdotdone.ddd.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.devdotdone.ddd.dto.tag.Tag;
+import com.devdotdone.ddd.dto.tag.TagType;
+/*
+CREATE TABLE tag (
+    tag_id        NUMBER PRIMARY KEY,
+    tagName          VARCHAR2(50) NOT NULL,
+    tagType          VARCHAR2(20)
+); 
+*/
 @Mapper
 public interface TagDao {
-  
+    // insertTag
+    public int insertTag(Tag tag);
+
+    // selectAllTags
+    public List<Tag> selectAllTags();
+    
+    // selectTagByTagType
+    public List<Tag> selectByTagType(String tagType);
+    
+    // updateTag
+    public int updateTag(Tag tag);
+
+    // deleteTag
+    public int deleteTag(int tagId);
 }
--- src/main/java/com/devdotdone/ddd/dao/UserTagDao.java
+++ src/main/java/com/devdotdone/ddd/dao/UserTagDao.java
@@ -1,8 +1,21 @@
 package com.devdotdone.ddd.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.devdotdone.ddd.dto.tag.Tag;
 
 @Mapper
 public interface UserTagDao {
+    // insertUserTags
+    int insertUserTag(@Param("userId") int userId, @Param("tagIds") List<Integer> tagIds);
+
+    // selectTagsByUser
+    List<Tag> selectByUserTag(int userId);
+
+    // deleteUserTags
+    int deleteUserTag(@Param("userId") int userId, @Param("tagIds") List<Integer> tagIds);
 
 }
