cde08c95a180988012a422809cad3a9ff421a802
Addilete <165387997+adillette@users.noreply.github.com>
2025-09-23 10:49:40 +0900

    feat: project 기본생성만 가능(멤버추가기능 없음)

--- src/main/java/com/devdotdone/ddd/controller/ProjectController.java
+++ src/main/java/com/devdotdone/ddd/controller/ProjectController.java
@@ -1,7 +1,12 @@
 package com.devdotdone.ddd.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devdotdone.ddd.dto.project.Project;
@@ -9,6 +14,7 @@ import com.devdotdone.ddd.service.ProjectService;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,15 +26,44 @@ public class ProjectController {
   @Autowired
   private ProjectService projectService;
 
-  @PostMapping("/project-create")
-  public Project projectCreate(@RequestBody Project project) {
+  @PostMapping("/create")
+  public Project create(@RequestBody Project project) {
       log.info(project.toString());
       
       projectService.create(project);
       Project dbProject= projectService.getProject(project.getProjectId());
-
+    
 
       return dbProject;
   }
+/*
+  @PostMapping("/update")
+  public Map<String, Object> update(@RequestBody Project project){
+    Project dbProject = projectService.update(project);
+
+    Map<String,Object> map = new HashMap<>();
+
+    if(dbProject==null){
+      map.put("result","fail");
+    }else{
+      map.put("result","success");
+      map.put("member",dbProject);
+    }
+    
+    return map;
+ 
+  }
+ */
+  @DeleteMapping("/delete")
+  public String delete(@RequestParam("projectId") int projectId){
+    projectService.delete(projectId);
+    JSONObject jsonObject= new JSONObject();
+    jsonObject.put("result","success");
+
+    return jsonObject.toString();//{"result":"success"}
+  }
+
+
+
   
 }
--- src/main/java/com/devdotdone/ddd/service/ProjectService.java
+++ src/main/java/com/devdotdone/ddd/service/ProjectService.java
@@ -2,6 +2,7 @@ package com.devdotdone.ddd.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.devdotdone.ddd.dao.ProjectDao;
 import com.devdotdone.ddd.dto.project.Project;
@@ -16,11 +17,34 @@ public class ProjectService {
 
   public void create(Project project){
     projectDao.insertProject(project);
+    log.info("insertProject 실행");
   }
 
   public Project getProject(int projectId){
     Project project = projectDao.selectProjectById(projectId);
     return project;
+  }
+
+  /* 
+  public Project update(Project project){
+
+    Project dbProject = projectDao.selectProjectById(project.getProjectId());
+    if(dbProject==null){
+      return null;
+    }else{
+      if(StringUtils.hasText(project.getProjectTitle())){
+        dbProject.setProjectTitle(project.getProjectTitle());
+      }
+      if(StringUtils.hasText(project.getProjectContent())){
+        dbProject.setProjectContent(project.getProjectContent());
+      }
+      
+    }
+  }
+*/
+  public int delete(int projectId){
+    int rows = projectDao.deleteProject(projectId);
+    return rows;
   }
 
   
