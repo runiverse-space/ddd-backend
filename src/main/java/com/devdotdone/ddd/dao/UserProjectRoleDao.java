991b2b38ea5a3681183fa312dc41f3c55ca25569
devbsu <devbsu@naver.com>
2025-09-23 16:34:20 +0900

    feat: userProjectRole mapper 수정

--- src/main/java/com/devdotdone/ddd/dao/UserProjectRoleDao.java
+++ src/main/java/com/devdotdone/ddd/dao/UserProjectRoleDao.java
@@ -9,32 +9,24 @@ import com.devdotdone.ddd.dto.userProjectRole.UserProjectRole;
 
 @Mapper
 public interface UserProjectRoleDao {
-  public int insertUsersProjectRoleAsAdmin(UserProjectRole userProjectRole);
-  public int insertUsersProjectRoleAsMember(UserProjectRole userProjectRole);
-  //특정 프로젝트 멤버 조회
-  public List<UserProjectRole> selectUsersByProjectId(int projectId);
-  //특정 유저의 프로젝트 목록 조회
-  public List<UserProjectRole> selectUsersByUserId(int userId);
-  //유저 권한 확인
-  public String selectUserRoleInProject(UserProjectRole userProjectRole);
-  //프로젝트에서 유저 제거
-  public int deleteUserFromProject(@Param("userId")int userId,@Param("projectId") int projectId);
-
-
   // insertUsersProjectRole
+  int insertUsersProjectRole(UserProjectRole userProjectRole);
 
   // selectProjectMembers
+  List<UserProjectRole> selectProjectMembers(@Param("projectId") int projectId);
 
   // countProjectMembers
+  int countProjectMembers(@Param("projectId") int projectId);
 
   // countProjectAdmins
+  int countProjectAdmins(@Param("projectId") int projectId);
 
   // selectUserProjectRole
-
-  // selectUserRoleInProject
+  UserProjectRole selecUserProjectRole(@Param("projectId") int projectId, @Param("userId") int userId);
 
   // updateUserProjectRole
+  int updateUserProjectRole(UserProjectRole userProjectRole);
 
   // deleteUserProjectRole
-
+  int deleteUserProjectRole(@Param("projectId") int projectId, @Param("userId") int userId);
 }
