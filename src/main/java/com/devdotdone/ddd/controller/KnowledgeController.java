a9c9f5ee748271bea3c06e0983c0b158e5d0681c
Addilete <165387997+adillette@users.noreply.github.com>
2025-09-24 19:57:14 +0900

    feat: 지식창고 컨트롤러 추가

--- src/main/java/com/devdotdone/ddd/controller/KnowledgeController.java
+++ src/main/java/com/devdotdone/ddd/controller/KnowledgeController.java
@@ -1,9 +1,16 @@
 package com.devdotdone.ddd.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,6 +18,11 @@ import com.devdotdone.ddd.dto.knowledge.Knowledge;
 import com.devdotdone.ddd.service.KnowledgeService;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @Slf4j
 @RestController
@@ -18,7 +30,9 @@ import lombok.extern.slf4j.Slf4j;
 public class KnowledgeController {
   @Autowired
   KnowledgeService knowledgeService;
-
+/*
+  지식창고 글 생성
+ */
   @PostMapping("/create")
   public Knowledge create(@ModelAttribute Knowledge knowledge) throws Exception{
      
@@ -37,6 +51,49 @@ public class KnowledgeController {
     
     return dbknowledge;
   }
+ 
+  /*
+   * 특정 지식창고 글 자세히보기
+   */
+  @GetMapping("/detail")
+  public Map<String, Object> detail(@RequestParam("knowledgeId") int knowledgeId) {
+     
+    Map<String, Object> resultMap= new HashMap<>();
+    Knowledge knowledge = knowledgeService.getKnowledge(knowledgeId);
+    resultMap.put("result","success");
+    resultMap.put("data",knowledge);
+    return resultMap;
+
+  }
+ /*
+  * 지식 창고 수정하기
+  */
+   @PutMapping("/update")
+   public Knowledge update(@RequestBody Knowledge knowledge) {
+       log.info("지식창고 수정하기",knowledge.toString());
+
+       knowledgeService.update(knowledge);//반환값 무시하고 
+
+       Knowledge dbKnowledge = knowledgeService.getKnowledge(knowledge.getKnowledgeId());
+     
+       return dbKnowledge;
+
+   }
+
+ /*
+  * 지식 창고 삭제하기
+  */
+
+  @DeleteMapping("/delete")
+  public String delete(@RequestParam("knowledgeId") int knowledgeId){
+    knowledgeService.delete(knowledgeId);
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("result", "success");
+
+    return jsonObject.toString();
+  }
+
+  
     
   
 }
