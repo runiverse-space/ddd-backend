ab0d4afb12c9a40abb362a8e75bc0c4939e0e5dd
Addilete <165387997+adillette@users.noreply.github.com>
2025-09-24 13:25:13 +0900

    feat: KnowledgeComment 기본 엔티티, request 객체 추가

--- src/main/java/com/devdotdone/ddd/dto/knowledge/KnowledgeComment.java
+++ src/main/java/com/devdotdone/ddd/dto/knowledge/KnowledgeComment.java
@@ -1,6 +1,7 @@
 package com.devdotdone.ddd.dto.knowledge;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 import lombok.Data;
 
@@ -11,4 +12,17 @@ public class KnowledgeComment {
     private int userId;
     private String knowledgeCommentContent;
     private Date knowledgeCommentCreatedAt;
+    //업데이트한 시간 추가함
+    private Date knowledgeCommentUpdatedAt;
+
+    //수정된 날짜 표기하는 메서드
+    public Date getDisplayDate(){
+        return knowledgeCommentUpdatedAt !=null ? knowledgeCommentUpdatedAt : knowledgeCommentCreatedAt;
+    }
+
+    public boolean isUpdated(){
+        return knowledgeCommentUpdatedAt !=null;
+    }
+    
+
 }
