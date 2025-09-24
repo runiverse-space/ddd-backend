388d6877eabb33b7aa5d2ce208a947d79baf5cf7
dlgudwn55 <dkrlekeheoek@naver.com>
2025-09-24 16:31:08 +0900

    chore: 메서드 이름 수정

--- src/main/java/com/devdotdone/ddd/dao/ScheduleMemberDao.java
+++ src/main/java/com/devdotdone/ddd/dao/ScheduleMemberDao.java
@@ -11,6 +11,7 @@ public interface ScheduleMemberDao {
   public void chargeUsers(ScheduleMember scheduleMember);
   public ScheduleMember selectOne(ScheduleMember scheduleMember);
   public List<ScheduleMember> findUsers(int scheduleId);
+  public List<ScheduleMember> findSchedulesOfUsers(int userId);
   public int dischargeUsers(ScheduleMember scheduleMember);
   public int dischargeAll(int scheduleId);
 }
