9fa193664e95a2d8b9354bb061e6d3995b7b9ea7
devbsu <devbsu@naver.com>
2025-09-25 18:41:07 +0900

    feat: 지식 태그 추가

---
+++ src/main/java/com/devdotdone/ddd/service/KnowledgeTagService.java
@@ -0,0 +1,54 @@
+package com.devdotdone.ddd.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.devdotdone.ddd.dao.KnowledgeTagDao;
+import com.devdotdone.ddd.dto.tag.KnowledgeTagRequest;
+import com.devdotdone.ddd.dto.tag.KnowledgeTagResponse;
+import com.devdotdone.ddd.dto.tag.Tag;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class KnowledgeTagService {
+    private final KnowledgeTagDao knowledgeTagDao;
+
+    public KnowledgeTagResponse create(KnowledgeTagRequest request) {
+        List<Tag> existing = knowledgeTagDao.selectTagByKnowledgeId(request.getKnowledgeId());
+        TagValidator.validate(existing.size(), request.getTagIds().size());
+
+        for (int tagId : request.getTagIds()) {
+            boolean alreadyExists = existing.stream().anyMatch(t -> t.getTagId() == tagId);
+
+            if (alreadyExists) {
+                throw new IllegalArgumentException("이미 등록된 태그입니다.");
+            }
+            knowledgeTagDao.insertKnowledgeTag(request.getKnowledgeId(), tagId);
+        }
+        return getKnowledgeTags(request.getKnowledgeId());
+    }
+
+    public KnowledgeTagResponse delete(KnowledgeTagRequest request) {
+        for (int tagId : request.getTagIds()) {
+            int rows = knowledgeTagDao.deleteKnowledgeTag(request.getKnowledgeId(), tagId);
+
+            if (rows == 0) {
+                throw new IllegalArgumentException("삭제할 태그가 존재하지 않습니다.");
+            }
+        }
+        return getKnowledgeTags(request.getKnowledgeId());
+    }
+
+    public KnowledgeTagResponse getKnowledgeTags(int knowledgeId) {
+        List<Tag> tags = knowledgeTagDao.selectTagByKnowledgeId(knowledgeId);
+
+        KnowledgeTagResponse response = new KnowledgeTagResponse();
+        response.setKnowledgeId(knowledgeId);
+        response.setTags(tags);
+
+        return response;
+    }
+}
