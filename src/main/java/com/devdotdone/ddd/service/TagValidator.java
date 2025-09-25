80abc5e749e908cac77798849be8b34ce725324e
devbsu <devbsu@naver.com>
2025-09-25 15:04:54 +0900

    feat: 태그 검증 클래스 분리

--- src/main/java/com/devdotdone/ddd/service/TagValidator.java
+++ src/main/java/com/devdotdone/ddd/service/TagValidator.java
@@ -1,35 +1,16 @@
 package com.devdotdone.ddd.service;
 
-import java.util.List;
+/**
+ * 공통 검증 클래스
+ * 태그는 최대 3개까지만 등록 가능
+ */
 
-import org.springframework.stereotype.Component;
-
-import com.devdotdone.ddd.dao.TagDao;
-import com.devdotdone.ddd.dto.tag.Tag;
-import com.devdotdone.ddd.dto.tag.TagType;
-
-import lombok.RequiredArgsConstructor;
-
-@Component
-@RequiredArgsConstructor
 public class TagValidator {
-    private final TagDao tagDao;
     private static final int MAX_TAGS = 3;
 
-    public void validate(List<Integer> tagIds, TagType expectedType) {
-        if (tagIds.size() > MAX_TAGS) {
-            throw new IllegalArgumentException("최대 3개까지 선택 가능합니다.");
-        }
-
-        List<Tag> tags = tagDao.selectTagById(tagIds);
-
-        if (tags.size() != tagIds.size()) {
-            throw new IllegalArgumentException("존재하지 않는 태그가 포함되어 있습니다.");
-        }
-
-        for (Tag tag : tags) {
-            if (tag.getTagType() != expectedType)
-                throw new IllegalArgumentException("태그 타입이 올바르지 않습니다.");
+    public static void validate(int existingCount, int newCount) {
+        if (existingCount + newCount > MAX_TAGS) {
+            throw new IllegalArgumentException("태그는 최대 " + MAX_TAGS + "개까지 선택 가능합니다.");
         }
     }
 }
