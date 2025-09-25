dfbaabd621ceda60be7dd405e645c7dfefc24ee6
devbsu <devbsu@naver.com>
2025-09-25 15:12:29 +0900

    chore: swagger config 수정

--- src/main/java/com/devdotdone/ddd/config/SwaggerConfig.java
+++ src/main/java/com/devdotdone/ddd/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
 public class SwaggerConfig {
 
@@ -13,5 +16,14 @@ public class SwaggerConfig {
                 .group("Dev-Dot-Done API")
                 .pathsToMatch("/api/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Dev-Dot-Done")
+                        .version("v1")
+                        .description("DDD API 명세서"));
     }
 }
