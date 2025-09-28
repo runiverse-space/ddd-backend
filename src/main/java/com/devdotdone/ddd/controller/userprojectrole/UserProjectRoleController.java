89ff7ddb6ac76a9add05f1f86ce020917de9ec31
Addilete <165387997+adillette@users.noreply.github.com>
2025-09-28 16:56:35 +0900

    feat: api의 확장성을 위해 UserProjectRoleController 추가함 해당 컨트롤러에서 기존프로젝트에 멤버 추가하기, 권한 위임, 멤버삭제, 목록조회 가능함

---
+++ src/main/java/com/devdotdone/ddd/controller/userprojectrole/UserProjectRoleController.java
@@ -0,0 +1,119 @@
+package com.devdotdone.ddd.controller.userprojectrole;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devdotdone.ddd.dto.userProjectRole.UserProjectRole;
+import com.devdotdone.ddd.service.UserProjectRoleService;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/userprojectrole")
+public class UserProjectRoleController {
+
+  @Autowired
+  private UserProjectRoleService userProjectRoleService;
+
+ 
+
+  // 프로젝트 멤버 목록 조회
+  @GetMapping("/list")
+  public List<UserProjectRole> userProjectRoleList(@RequestParam("projectId") int projectId) {
+
+    List<UserProjectRole> projectmemberlist = userProjectRoleService.getProjectMember(projectId);
+    log.info(projectmemberlist.toString());
+
+    return projectmemberlist;
+
+  }
+
+  
+
+  // 새로운 멤버 영입(총인원 6명인 조건 그대로)
+  @PostMapping("/insertmembers")
+  public Map<String, String> addMember(@RequestBody UserProjectRole userProjectRole) {
+   
+      userProjectRoleService.assignUsersToProject(
+          userProjectRole.getProjectId(),
+          userProjectRole.getUserId(),
+          userProjectRole.getUprRole());
+  
+
+    System.out.println("프로젝트 ID: " + userProjectRole.getProjectId());
+    System.out.println("사용자 ID: " + userProjectRole.getUserId());
+    System.out.println("역할: " + userProjectRole.getUprRole());
+
+    String role = userProjectRoleService.getUserProjectRole(userProjectRole.getProjectId(),
+        userProjectRole.getUserId());
+    log.info(role.toString());
+    Map<String, String> map = new HashMap<>();
+    map.put("result", "success");
+    map.put("role", role);
+    return map;
+  }
+
+
+  // 팀장 권한 위임
+  @PostMapping("/changeadmin")
+  public Map<String,Object> changeAdmin(@RequestBody UserProjectRole userProjectRole){
+    
+    Map<String,Object> response = new HashMap<>();
+
+    try {
+      int result= userProjectRoleService.updateAdmin(userProjectRole);
+      response.put("suceess",true);
+      response.put("result",result);
+    
+    } catch (Exception e) {
+      response.put("suceess",false);
+      response.put("result",0);
+    }
+    
+    return response;
+      
+     
+  }
+  
+  // 프로젝트 멤버 수 조회
+  @GetMapping("/count")
+  public Map<String,Object> countProjectMembers(@RequestParam("projectId") int projectId){
+    Map<String,Object> result= new HashMap<>()  ;
+    int total =userProjectRoleService.getCountProjectMembers(projectId);
+    log.info("프로젝트 인원 총 {}",total+" 명입니다.");
+    result.put("success",true);
+    result.put("total", total);
+    
+    return result;
+
+  }
+
+  // 프로젝터에서 멤버 빼기
+  @DeleteMapping("/delete")
+  public String deleteMembers(@RequestParam("projectId")int projectId,@RequestParam("userId") int userId){
+    userProjectRoleService.deleteUsersFromProject(projectId,userId );
+
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("result", "success");
+
+    return jsonObject.toString();
+
+  }
+
+
+
+}
