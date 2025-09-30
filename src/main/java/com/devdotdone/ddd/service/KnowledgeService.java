c8d619499521ad3bd1b2ad70f83c0e6561183df8
Addilete <165387997+adillette@users.noreply.github.com>
2025-09-30 15:29:31 +0900

    feat: 지식창고 목록 추가용 서비스 추가함

--- src/main/java/com/devdotdone/ddd/service/KnowledgeService.java
+++ src/main/java/com/devdotdone/ddd/service/KnowledgeService.java
@@ -1,5 +1,7 @@
 package com.devdotdone.ddd.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +22,20 @@ public class KnowledgeService {
     knowledgeDao.insertKnowledge(knowledge);
   }
 
-  //첨부 파일 없는거
+  //첨부 파일 없는거 1개 조회
   public Knowledge getKnowledge(int knowledgeId){
     Knowledge knowledge = knowledgeDao.selectKnowledgeByKnowledgeId(knowledgeId);
     return knowledge;
+  }
+
+  //특정 project의 지식창고 목록 조회
+  public List<Knowledge> getKnowledgeListByProject(int projectId){
+    return knowledgeDao.selectKnowledgeByProjectId(projectId);
+  }
+
+  //특정 user의 지식창고 목록 조회
+  public List<Knowledge> getKnowledgesListByUser(int userId){
+    return knowledgeDao.selectKnowledgeByUserId(userId);
   }
 
   public int update(Knowledge knowledge){
