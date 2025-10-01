daa4cae16800b53e78eb214502051fd9db3fc432
dlgudwn55 <dkrlekeheoek@naver.com>
2025-10-01 09:41:31 +0900

    chore: implements 구문 삭제

--- src/main/java/com/devdotdone/ddd/controller/knowledge/KnowledgeCommentController.java
+++ src/main/java/com/devdotdone/ddd/controller/knowledge/KnowledgeCommentController.java
@@ -23,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/api/knowledgecomment")
-public class KnowledgeCommentController implements KnowledgeCommentDocs{
+public class KnowledgeCommentController {
   
   @Autowired
   private KnowledgeCommentService knowledgeCommentService;
